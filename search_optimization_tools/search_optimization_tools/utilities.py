07a90fb5d648b26dd870652635c66fc0369245b9
Yinan <yinan@dropoff.ai>
2022-08-14 23:04:54 -0400

    Added straightline and haversine to utilities

--- search_optimization_tools/search_optimization_tools/utilities.py
+++ search_optimization_tools/search_optimization_tools/utilities.py
@@ -1,3 +1,4 @@
+import math
 '''
 paths: List of Leaflet AntPaths
 
@@ -7,3 +8,19 @@ def get_paths_bounds(paths):
     lats = [p.locations[0][0] for p in paths]
     lngs = [p.locations[0][1] for p in paths]
     return [[min(lats),min(lngs)],[max(lats),max(lngs)]]
+
+def straight_line(lon1, lat1, lon2, lat2):
+    return math.sqrt((lon2 - lon1)**2 + (lat2-lat1)**2)
+
+def haversine_distance(lon1, lat1, lon2, lat2):
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.asin(math.sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
