5ab3f6a085a89456cc800af38422f6f8e23edb48
Yinan <yinan@dropoff.ai>
2022-08-14 23:04:13 -0400

    Formatting + Added A*

--- search_optimization_tools/search_optimization_tools/algorithms.py
+++ search_optimization_tools/search_optimization_tools/algorithms.py
@@ -7,21 +7,23 @@ from sys import getsizeof
 from copy import deepcopy
 import networkx as nx
 
-'''
+"""
 Requirements:
 The node class must have the following public object methods:
 - path(): generates a path from origin to this node by tracing node parents
 - expand(): generates a list of all the children (reachable nodes) from this node
-'''
+"""
+
+
 def BFS(origin, destination):
 
-    time_start = process_time() # Time tracking
-    max_frontier = 0 # Space tracking
+    time_start = process_time()  # Time tracking
+    max_frontier = 0  # Space tracking
     route = []
     frontier = deque([origin])
-    explored = set() # Explored tracking
+    explored = set()  # Explored tracking
     found = False
-    
+
     while frontier and not found:
         node = frontier.popleft()
         explored.add(node)
@@ -31,27 +33,29 @@ def BFS(origin, destination):
                     route = child.path()
                     found = True
                 frontier.append(child)
-                if getsizeof(frontier)> max_frontier:
+                if getsizeof(frontier) > max_frontier:
                     max_frontier = getsizeof(frontier)
 
-    time_end = process_time() # Time tracking
+    time_end = process_time()  # Time tracking
 
-    return Solution(route, time_end-time_start, max_frontier, len(explored))
+    return Solution(route, time_end - time_start, max_frontier, len(explored))
 
 
-'''
+"""
 Requirements:
 The node class must have the following public object methods:
 - path(): generates a path from origin to this node by tracing node parents
 - expand(): generates a list of all the children (reachable nodes) from this node
-'''
+"""
+
+
 def DFS(origin, destination):
-    time_start = process_time() # Time tracking
-    max_frontier = 0 # Space tracking
+    time_start = process_time()  # Time tracking
+    max_frontier = 0  # Space tracking
 
     route = []
     frontier = deque([origin])
-    explored = set() # Explored tracking
+    explored = set()  # Explored tracking
     found = False
 
     while frontier and not found:
@@ -63,13 +67,13 @@ def DFS(origin, destination):
                     route = child.path()
                     found = True
                 frontier.append(child)
-                if getsizeof(frontier)> max_frontier:
+                if getsizeof(frontier) > max_frontier:
                     max_frontier = getsizeof(frontier)
-    time_end = process_time() # Time tracking
-    return Solution(route, time_end-time_start, max_frontier, len(explored))
+    time_end = process_time()  # Time tracking
+    return Solution(route, time_end - time_start, max_frontier, len(explored))
 
 
-'''
+"""
 Requirements:
 The node class must have the following public object methods:
 - path(): generates a path from origin to this node by tracing node parents
@@ -80,13 +84,15 @@ The node class must have the following public object methods:
 - set_distance(distance): sets the edge distance between this node and its parent
 
 
-'''
+"""
+
+
 def Dijkstra(origin, destination, unrelaxed_nodes):
-    time_start = process_time() # Time tracking
-    space = getsizeof(deepcopy(unrelaxed_nodes)) # Space tracking
+    time_start = process_time()  # Time tracking
+    space = getsizeof(deepcopy(unrelaxed_nodes))  # Space tracking
 
     # Using a set here avoids the problem with self loops
-    seen = set() # explored tracking
+    seen = set()  # explored tracking
     shortest_dist = {node.get_id(): math.inf for node in unrelaxed_nodes}
     shortest_dist[origin.get_id()] = 0
     found = False
@@ -107,27 +113,29 @@ def Dijkstra(origin, destination, unrelaxed_nodes):
             if child.get_id() in seen:
                 continue
             child_obj = next(
-                (node for node in unrelaxed_nodes if node.get_id() == child.get_id()), None
+                (node for node in unrelaxed_nodes if node.get_id() == child.get_id()),
+                None,
             )
             child_obj.set_distance(child.get_distance())
             distance = shortest_dist[node.get_id()] + child.get_distance()
             if distance < shortest_dist[child_obj.get_id()]:
                 shortest_dist[child_obj.get_id()] = distance
                 child_obj.set_parent(node)
-    time_end = process_time() # Time tracking
-    return Solution(route, time_end-time_start, space, len(seen))
+    time_end = process_time()  # Time tracking
+    return Solution(route, time_end - time_start, space, len(seen))
+
 
 # This implementation uses a heap with tuples (a,b),
 # a is the cost of a node, and b is the node itself.
 def UCS(origin, destination):
-    time_start = process_time() # Time tracking
-    max_priority = 0 # Space tracking
+    time_start = process_time()  # Time tracking
+    max_priority = 0  # Space tracking
 
     entry_count = 1
     priority_queue = [(0, 0, origin)]
 
     found = False
-    visited = [] # Explored tracking
+    visited = []  # Explored tracking
     while priority_queue and not found:
         node = heapq.heappop(priority_queue)
         node_cost = node[0]
@@ -155,14 +163,15 @@ def UCS(origin, destination):
                     heapq.heapify(priority_queue)
             else:
                 heapq.heappush(priority_queue, (total_cost, entry_count, child))
-                if getsizeof(priority_queue)> max_priority:
+                if getsizeof(priority_queue) > max_priority:
                     max_priority = getsizeof(priority_queue)
                 entry_count += 1
-    time_end = process_time() # Time tracking
-    return Solution(route, time_end-time_start, max_priority, len(visited))
+    time_end = process_time()  # Time tracking
+    return Solution(route, time_end - time_start, max_priority, len(visited))
+
 
 def Bidirectional_Dijkstra(origin, destination, unrelaxed_nodes):
-    time_start = process_time() # Time tracking
+    time_start = process_time()  # Time tracking
     frontier = deepcopy(unrelaxed_nodes)
 
     space_required = getsizeof(frontier)
@@ -179,10 +188,10 @@ def Bidirectional_Dijkstra(origin, destination, unrelaxed_nodes):
     found = False
     route = []
 
-    altr_expand = True # to alternate between front and back
+    altr_expand = True  # to alternate between front and back
 
     while frontier and not found:
-        if altr_expand: # Forward
+        if altr_expand:  # Forward
             node = min(frontier, key=lambda node: shortest_dist_f[node.get_id()])
             # relaxing the node, so this node's value in shortest_dist is the shortest distance between the origin and destination
             frontier.remove(node)
@@ -216,7 +225,7 @@ def Bidirectional_Dijkstra(origin, destination, unrelaxed_nodes):
                     shortest_dist_f[child_obj.get_id()] = distance
                     child_obj.set_parent(node)
             altr_expand = False
-        if not altr_expand: # Backward
+        if not altr_expand:  # Backward
             node = min(frontier, key=lambda node: shortest_dist_b[node.get_id()])
             # relaxing the node, so this node's value in shortest_dist is the shortest distance between the origin and destination
             frontier.remove(node)
@@ -235,7 +244,9 @@ def Bidirectional_Dijkstra(origin, destination, unrelaxed_nodes):
 
                 # Check the child is collided
                 if child in explored_f:
-                    overlapped = next((node for node in explored_f if node == child), None)
+                    overlapped = next(
+                        (node for node in explored_f if node == child), None
+                    )
                     route = overlapped.path()[:-1] + child.path()[::-1]
                     found = True
                     break
@@ -249,10 +260,13 @@ def Bidirectional_Dijkstra(origin, destination, unrelaxed_nodes):
                     shortest_dist_b[child_obj.get_id()] = distance
                     child_obj.set_parent(node)
             altr_expand = True
-    time_end = process_time() # Time tracking
-    return Solution(route, time_end - time_start, space_required, len(explored_f)+len(explored_b))
+    time_end = process_time()  # Time tracking
+    return Solution(
+        route, time_end - time_start, space_required, len(explored_f) + len(explored_b)
+    )
 
-def Kruskal(G, attr = "weight", sorted_edges = False, edges = None, graph_type=nx.Graph):
+
+def Kruskal(G, attr="weight", sorted_edges=False, edges=None, graph_type=nx.Graph):
     if not sorted_edges:
         edges = sorted(G.edges(data=True), key=lambda t: t[2].get(attr, 1))
     mst = graph_type()
@@ -271,3 +285,39 @@ def Kruskal(G, attr = "weight", sorted_edges = False, edges = None, graph_type=n
                     break
             continue
     return mst
+
+
+"""
+Requirements:
+The node class must have the following public object methods:
+- path(): generates a path from origin to this node by tracing node parents
+- expand(): generates a list of all the children (reachable nodes) from this node
+"""
+
+
+def A_Star(
+    G, origin, destination, heuristic_fn, cost_fn, cost_kwargs=[], expand_kwargs=[]
+):
+    toDestination = heuristic_fn(G, origin, destination)
+    toOrigin = {}
+    route = []
+    frontier = list()
+    frontier.append(origin)
+    toOrigin[origin] = 0
+    explored = set()
+    found = False
+    while frontier and not found:
+        # choose a node based on its heuristic value
+        node = min(frontier, key=lambda node: toOrigin[node] + toDestination[node])
+        frontier.remove(node)
+        explored.add(node)
+        # expand its children
+        for child in node.expand(**expand_kwargs):
+            if child not in explored and child not in frontier:
+                if child == destination:
+                    route = child.path()
+                    found = True
+                    continue
+                frontier.append(child)
+                toOrigin[child] = cost_fn(G, child.path(), **cost_kwargs)
+    return route
