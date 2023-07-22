8d23b5015222b8e78ac9a42959fce2a1f3327686
Dr-AlaaKhamis <alaakhamis@gmail.com>
2023-07-22 11:18:14 -0400

    update

--- Chapter 11/som_tsp/README.md
+++ Chapter 11/som_tsp/README.md
@@ -1,5 +1,3 @@
-# Solving the Traveling Salesman Problem using Self-Organizing Maps
-
-These files are from [Solving the Traveling Salesman Problem using Self-Organizing Maps](https://github.com/diego-vicente/som-tsp) GitHub repo.
+These helper functions are from [som-tsp](https://github.com/diego-vicente/som-tsp) GitHub repo.
 
 Copyright (c) 2018 Diego Vicente
---
+++ Chapter 11/som_tsp/helper.py
@@ -0,0 +1,139 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+def read_tsp(filename):
+    """
+    Read a file in .tsp format into a pandas DataFrame
+
+    The .tsp files can be found in the TSPLIB project. Currently, the library
+    only considers the possibility of a 2D map.
+    """
+    with open(filename) as f:
+        node_coord_start = None
+        dimension = None
+        lines = f.readlines()
+
+        # Obtain the information about the .tsp
+        i = 0
+        while not dimension or not node_coord_start:
+            line = lines[i]
+            if line.startswith('DIMENSION :'):
+                dimension = int(line.split()[-1])
+            if line.startswith('NODE_COORD_SECTION'):
+                node_coord_start = i
+            i = i+1
+
+        print('Problem with {} cities read.'.format(dimension))
+
+        f.seek(0)
+
+        # Read a data frame out of the file descriptor
+        cities = pd.read_csv(
+            f,
+            skiprows=node_coord_start + 1,
+            sep=' ',
+            names=['city', 'y', 'x'],
+            dtype={'city': str, 'x': np.float64, 'y': np.float64},
+            header=None,
+            nrows=dimension
+        )
+        return cities
+    
+def normalize(points):
+    """
+    Return the normalized version of a given vector of points.
+
+    For a given array of n-dimensions, normalize each dimension by removing the
+    initial offset and normalizing the points in a proportional interval: [0,1]
+    on y, maintining the original ratio on x.
+    """
+    ratio = (points.x.max() - points.x.min()) / (points.y.max() - points.y.min()), 1
+    ratio = np.array(ratio) / max(ratio)
+    norm = points.apply(lambda c: (c - c.min()) / (c.max() - c.min()))
+    return norm.apply(lambda p: ratio * p, axis=1)
+
+def get_neighborhood(center, radix, domain):
+    """Get the range gaussian of given radix around a center index."""
+
+    # Impose an upper bound on the radix to prevent NaN and blocks
+    if radix < 1:
+        radix = 1
+
+    # Compute the circular network distance to the center
+    deltas = np.absolute(center - np.arange(domain))
+    distances = np.minimum(deltas, domain - deltas)
+
+    # Compute Gaussian distribution around the given center
+    return np.exp(-(distances*distances) / (2*(radix*radix)))
+
+def euclidean_distance(a, b):
+    """Return the array of distances of two numpy arrays of points."""
+    return np.linalg.norm(a - b, axis=1)
+
+def select_closest(candidates, origin):
+    """Return the index of the closest candidate to a given point."""
+    return euclidean_distance(candidates, origin).argmin()
+
+def get_route(cities, network):
+    """Return the route computed by a network."""
+    cities['winner'] = cities[['x', 'y']].apply(
+        lambda c: select_closest(network, c),
+        axis=1, raw=True)
+
+    return cities.sort_values('winner').index
+
+def route_distance(cities):
+    """Return the cost of traversing a route of cities in a certain order."""
+    points = cities[['x', 'y']]
+    distances = euclidean_distance(points, np.roll(points, 1, axis=0))
+    return np.sum(distances)
+
+def plot_network(cities, neurons, name='diagram.png', ax=None):
+    """Plot a graphical representation of the problem"""
+    mpl.rcParams['agg.path.chunksize'] = 10000
+
+    if not ax:
+        fig = plt.figure(figsize=(5, 5), frameon = False)
+        axis = fig.add_axes([0,0,1,1])
+
+        axis.set_aspect('equal', adjustable='datalim')
+        plt.axis('off')
+
+        axis.scatter(cities['x'], cities['y'], color='red', s=4)
+        axis.plot(neurons[:,0], neurons[:,1], 'r.', ls='-', color='#0063ba', markersize=2)
+
+        #plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close()
+
+    else:
+        ax.scatter(cities['x'], cities['y'], color='red', s=4)
+        ax.plot(neurons[:,0], neurons[:,1], 'r.', ls='-', color='#0063ba', markersize=2)
+        return ax
+
+def plot_route(cities, route, name='diagram.png', ax=None):
+    """Plot a graphical representation of the route obtained"""
+    mpl.rcParams['agg.path.chunksize'] = 10000
+
+    if not ax:
+        fig = plt.figure(figsize=(5, 5), frameon = False)
+        axis = fig.add_axes([0,0,1,1])
+
+        axis.set_aspect('equal', adjustable='datalim')
+        plt.axis('off')
+
+        axis.scatter(cities['x'], cities['y'], color='red', s=4)
+        route = cities.reindex(route)
+        route.loc[route.shape[0]] = route.iloc[0]
+        axis.plot(route['x'], route['y'], color='purple', linewidth=1)
+        plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close()
+
+    else:
+        ax.scatter(cities['x'], cities['y'], color='red', s=4)
+        route = cities.reindex(route)
+        route.loc[route.shape[0]] = route.iloc[0]
+        ax.plot(route['x'], route['y'], color='purple', linewidth=1)
+        return ax
